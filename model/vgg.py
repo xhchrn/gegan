470854e4a99f2d455d7800bbfb8d32afd150a1e0
Johnson Chen <xhchrn@gmail.com>
2017-05-16 23:19:14 +0800

    modified vgg model

--- model/vgg.py
+++ model/vgg.py
@@ -6,50 +6,54 @@ from scipy.io import loadmat
 
 class VGG_Model(object):
     def __init__(self):
-        self.param_path = os.path.join(os.getcwd(), "model", "vgg-face.mat")
-        self.build_model()
+        self.param_path    = os.path.join(os.getcwd(), "model", "vgg-face.mat")
+        self.data          = loadmat(self.param_path)
+        self.meta          = self.data['meta']
+        self.classes       = self.meta['classes']
+        self.class_names   = self.classes[0][0]['description'][0][0]
+        self.normalization = self.meta['normalization']
+        self.layers        = self.data['layers']
+        self.average_image = np.squeeze(normalization[0][0]['averageImage'][0][0][0][0])
+        self.image_size    = np.squeeze(normalization[0][0]['imageSize'][0][0])
 
-    def build_model(self):
+        self.used = False
+
+    def vgg(self, input_maps, reuse=False):
         with tf.variable_scope("vgg"):
-            data = loadmat(self.param_path)
+            if reuse:
+                tf.get_variable_scope().reuse_variables()
 
-            self.input_maps = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 3], name="input_maps")
-
-            # read meta info
-            meta          = data['meta']
-            classes       = meta['classes']
-            class_names   = classes[0][0]['description'][0][0]
-            normalization = meta['normalization']
-            average_image = np.squeeze(normalization[0][0]['averageImage'][0][0][0][0])
-            image_size    = np.squeeze(normalization[0][0]['imageSize'][0][0])
-            input_maps    = tf.image.resize_images(self.input_maps, size=[image_size[0], image_size[1]])
+            input_maps = input_maps - tf.constant(self.average_image)
+            input_maps = tf.image.resize_images(input_maps, size=[self.image_size[0], self.image_size[1]])
 
             # read layer info
-            layers = data['layers']
             current = input_maps
             network = {}
-            for layer in layers[0]:
-                name = layer[0]['name'][0][0]
-                layer_type = layer[0]['type'][0][0]
+            for layer in self.layers[0]:
+                name = self.layer[0]['name'][0][0]
+                layer_type = self.layer[0]['type'][0][0]
                 if layer_type == 'conv':
                     if name[:2] == 'fc':
                         padding = 'VALID'
                     else:
                         padding = 'SAME'
-                    stride = layer[0]['stride'][0][0]
-                    kernel, bias = layer[0]['weights'][0][0]
-                    # kernel = np.transpose(kernel, (1, 0, 2, 3))
-                    bias = np.squeeze(bias).reshape(-1)
-                    conv = tf.nn.conv2d(current, tf.constant(kernel),
+                    stride = self.layer[0]['stride'][0][0]
+                    kernel, bias = self.layer[0]['weights'][0][0]
+                    bias   = np.squeeze(bias).reshape(-1)
+                    kernel = tf.constant(kernel)
+                    bias   = tf.constant(bias)
+                    kernel = tf.get_variable(name+"_W", initializer=kernel)
+                    bias   = tf.get_variable(name+"_b", initializer=bias)
+                    conv   = tf.nn.conv2d(current, kernel,
                                         strides=(1, stride[0], stride[0], 1), padding=padding)
                     current = tf.nn.bias_add(conv, bias)
-                    print(name, 'stride:', stride, 'kernel size:', np.shape(kernel))
+                    print(name, 'stride:', stride, 'kernel size:', tf.shape(kernel))
                 elif layer_type == 'relu':
                     current = tf.nn.relu(current)
                     print(name)
                 elif layer_type == 'pool':
-                    stride = layer[0]['stride'][0][0]
-                    pool = layer[0]['pool'][0][0]
+                    stride = self.layer[0]['stride'][0][0]
+                    pool = self.layer[0]['pool'][0][0]
                     current = tf.nn.max_pool(current, ksize=(1, pool[0], pool[1], 1),
                                              strides=(1, stride[0], stride[0], 1), padding='SAME')
                     print(name, 'stride:', stride)
@@ -59,20 +63,16 @@ class VGG_Model(object):
 
                 network[name] = current
 
-        setattr(self, "network", network)
-        setattr(self, "average_image", average_image)
-        setattr(self, "conv4", network["conv4_3"])
-        setattr(self, "conv5", network["conv5_3"])
+        return network["conv4_3"], network["conv5_3"]
 
-    def get_vgg_loss(self, sess, a, b):
-        a = a - self.average_image
-        b = b - self.average_image
+    def vgg_loss(self, a, b):
+        if self.used == False
+            conv4_a, conv5_a = self.vgg(a, reuse=False)
+            self.used = True
+        else:
+            conv4_a, conv5_a = self.vgg(a, reuse=True)
 
-        [conv4_a, conv5_a] = sess.run([self.conv4, self.conv5], feed_dict={self.input_maps: a})
-        [conv4_b, conv5_b] = sess.run([self.conv4, self.conv5], feed_dict={self.input_maps: b})
+        conv4_b, conv5_b = self.vgg(b, reuse=True)
 
-        loss_4 = np.mean(np.abs(conv4_a - conv4_b))
-        loss_5 = np.mean(np.abs(conv5_a - conv5_b))
-        loss = loss_4 + loss_5
-
-        return loss
+        return tf.reduce_mean(tf.abs(conv4_a - conv4_b)) + \
+               tf.reduce_mean(tf.abs(conv5_a - conv5_b))
